13928d0b453a9bac8908e1cd2c28d9c60082a97c
Diogo <diogobelo69@gmail.com>
2023-05-25 09:10:55 -0300

    teste

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,6 @@
 function typeWriter(elemento) {
   const textoArray = elemento.innerHTML.split("");
+  console.log(textoArray)
   elemento.innerHTML = "";
   textoArray.forEach((letra, i) => {
     setTimeout(() => (elemento.innerHTML += letra), 75 * i);
@@ -19,6 +20,7 @@ function timeContent(){
 const titulo = document.querySelector(".first-title");
 typeWriter(document.querySelector(".first-title"));
 timeContent()
+
 
 
 
